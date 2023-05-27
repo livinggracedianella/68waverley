683093a233587cf45eadbc575acf7c1141390784
Stone Breaker <8778605+breaker-of-stone@users.noreply.github.com>
2023-05-27 18:46:46 +0800

    Working grid for Events

--- websites/livinggracedianella/src/components/EventGrid.jsx
+++
@@ -1,8 +0,0 @@
-import EventGridItem from './EventGridItem';
-export default function EventGrid({ events }) {
-    return (
-        <div className="grid grid-cols-3 gap-6">
-            {events && events.length > 0 ? events.map((event) => <EventGridItem event={event} />) : 'No events found'}
-        </div>
-    );
-}
