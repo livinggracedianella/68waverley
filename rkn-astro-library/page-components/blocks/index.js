b897312c657985067c58bcec18be67976a3ac804
Rohan Nelson <rohan.nelson@gmail.com>
2023-10-05 16:26:29 +0800

    basic event pages and event index page implement - images yet to be added.

--- rkn-astro-library/page-components/blocks/index.js
+++ rkn-astro-library/page-components/blocks/index.js
@@ -4,7 +4,7 @@ export { default as FeatureEvent } from './FeatureEvent.astro'
 export { default as FeatureMessage } from './FeatureMessage.astro'
 export { default as FullWidth } from './FullWidth.astro'
 export { default as GatheringsGrid } from './GatheringsGrid.astro'
-export { default as PageHeader } from './PageHeader.astro'
+export { default as Header } from './Header.astro'
 export { default as RichText } from './RichText.astro'
 export { default as TwoColumn } from './TwoColumn.astro'
 
