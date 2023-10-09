e07ab4d6b2a614fc1838eba379e2ef5ed888ab9b
Rohan Nelson <rohan.nelson@gmail.com>
2023-10-09 14:50:38 +0800

    Tidied up a little - still builds, and I don't think I broke anything...

--- rkn-astro-library/base-components/index.js
+++ rkn-astro-library/base-components/index.js
@@ -1,7 +1,5 @@
-export { default as FooterWrapper } from './footer/FooterWrapper.astro'
 export { default as IconFacebook } from './icons/IconFacebook.astro'
 export { default as IconInstagram } from './icons/IconInstagram.astro'
-export { default as FooterNavItem } from './footer/FooterNavItem.astro'
 export { default as IconEmail } from './icons/IconEmail.astro'
 export { default as BaseLayout } from './BaseLayout.astro'
 export { default as NavigationBar } from './NavigationBar.astro'
