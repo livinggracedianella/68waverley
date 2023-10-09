a5c73fde847e2c37964ab76551f7d5053a2b6857
Rohan Nelson <rohan.nelson@gmail.com>
2023-10-09 16:46:13 +0800

    basic contact page implemented, include profiles grid component. Yet to implement single page profiles

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -1,5 +1,4 @@
 export { default as BlockSingleColumn } from './BlockSingleColumn.astro'
-export { default as GoogleMaps } from './GoogleMaps.astro'
 export { default as BlockDoubleColumn } from './BlockDoubleColumn.astro'
 export { default as SectionWrapper } from './SectionWrapper.astro'
 export { default as BlockPaddingWrapper } from './BlockPaddingWrapper.astro'
