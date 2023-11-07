0ccb5d895db74d15458c0e411798183bd3637b55
Jason O'Neil <jason.oneil@gmail.com>
2023-11-07 15:15:56 +0800

    Finish feature messages, add a button

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -2,6 +2,8 @@ export { default as BlockSingleColumn } from './BlockSingleColumn.astro'
 export { default as BlockDoubleColumn } from './BlockDoubleColumn.astro'
 export { default as SectionWrapper } from './SectionWrapper.astro'
 export { default as BlockPaddingWrapper } from './BlockPaddingWrapper.astro'
+export { default as Button } from './Button.astro'
 export * from './blocks'
 export * from './util'
 export * from './layout-components'
+
