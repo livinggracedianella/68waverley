62ed6ad3db1ccda31d91692bd17506d082e56dd9
Jason O'Neil <jason.oneil@gmail.com>
2023-11-07 16:01:48 +0800

    Remove unused components

--- rkn-astro-library/page-components/index.js
+++ rkn-astro-library/page-components/index.js
@@ -1,7 +1,3 @@
-export { default as BlockSingleColumn } from './BlockSingleColumn.astro'
-export { default as BlockDoubleColumn } from './BlockDoubleColumn.astro'
-export { default as SectionWrapper } from './SectionWrapper.astro'
-export { default as BlockPaddingWrapper } from './BlockPaddingWrapper.astro'
 export { default as Button } from './Button.astro'
 export * from './blocks'
 export * from './util'
