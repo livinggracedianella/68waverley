515b5ecbdb7e5bfe280e28f2ab70d6cc1f9b1b86
Jason O'Neil <jason.oneil@gmail.com>
2023-11-15 13:47:17 +0800

    Add cloudflare adapter

--- websites/68waverley/astro.config.mjs
+++ websites/68waverley/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  output: 'server',
+  adapter: cloudflare(),
 })
